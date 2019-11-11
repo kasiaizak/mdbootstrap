25ffdc590ae5700327f13ab5913e0effabdbe766
Kasia Izak <kontakt@kasiaizak.pl>
2019-11-11 13:53:19 +0100

    fix: remove console.logs

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,10 @@
 "use strict";
 
 let position = 1;
-console.log(position)
 
 $('.button__add--js').on('click', () => {
   $('.todo-list--js').append($(`<li class="todo-list__item todo-list__item--js list-group-item">Task number ${position}</li>`));
   position = position + 1;
-  console.log(position)
 })
 
 $('.button__remove--js').on('click', () => {
@@ -14,11 +12,9 @@ $('.button__remove--js').on('click', () => {
   if (position > 1) {
     position = position - 1;
   } else position = 1;
-  console.log(position)
 })
 
 $('.button__clear--js').on('click', () => {
   $('todo-list--js').empty();
   position = 1;
-  console.log(position)
 })
